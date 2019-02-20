d04b9790516a214e305a17f3cdccf839f8fca20f
alex <smirnoff.awa@gmail.com>
2019-02-20 09:21:19 +0100

    notre version

--- filterRestaurant.js
+++ filterRestaurant.js
@@ -3,17 +3,16 @@ fs.readFile('./restaurants.json', (err, data) => {
     if (err)
         console.log(err);
     else {
-        filterprudence=[]
         filterJson = []
-        let json = JSON.parse(data)
+        JSON
+            .parse(data)
             .map(e => {
-                if (e.name && e.annotation && e.image_url && e.address2 && e.address1 && e.longitude && e.latitude !== undefined) filterJson
-                    .push([e.name, e.annotation, e.image_url, e.address2, e.address1, e.latitude,e.longitude])
+                
+                if (e.name && e.annotation && e.image_url && e.address2 && e.address1 && e.longitude && e.latitude && e.editorial_rating  !== undefined) filterJson
+                    .push([e.name, e.annotation, e.image_url, e.address2, e.address1, e.latitude, e.longitude, e.editorial_rating])
             });
-
-
-        console.log(filterJson)
-    };
+            console.log(filterJson)
+        };
 
 }
 )
