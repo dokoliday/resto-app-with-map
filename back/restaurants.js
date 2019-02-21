81fcf742fe0caaa63f127cfc3e88f382f2fd5105
alex <smirnoff.awa@gmail.com>
2019-02-21 09:43:11 +0100

    creation des json utilise en base de donnees

--- back/restaurants.js
+++ back/restaurants.js
@@ -2,7 +2,7 @@ const connection = require('./conf.js');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/allRestaurants', (req, res) => {
     connection.query('SELECT * FROM restaurants_cleaned', (err, results) => {
         if(err){
             res.status(500).send(err)
@@ -11,6 +11,16 @@ router.get('/', (req, res) => {
         }
     })
 })
-router.
+
+router.get('/allAreas', (req, res) => {
+    connection.query('SELECT * FROM areas_cleaned', (err, results) => {
+        if(err){
+            res.status(500).send(err)
+        } else {
+            res.status(200).json(results);
+        }
+    })
+})
+
 
 module.exports = router;
--- filterRestaurant.js
+++ filterRestaurant.js
@@ -1,13 +1,20 @@
-const { get,uniqWith,isEqual } = require('lodash');
+//recuperation des fonctionnalitees lodash utilisees
+//get permet de rentrer en profondeur dans un objet
+//uniqwith assambler a isequal permet d'enlever les doublons dans un tableau d'objet 
+const { get, uniqWith, isEqual } = require('lodash');
+
+//write file permet d'editer un fichier, ici un json
 const { writeFile } = require('fs-promise')
+
+//import du fichier ./restaurant .json
 const restaurants = require('./restaurants.json');
 
-const restaurantsCleaned = restaurants
+//recuperation des donnees utiles dans json
+const restaurantJsonFiltrer = restaurants
   .map(restaurant => ({
     id: restaurant.id,
     name: restaurant.name,
     address: restaurant.address1,
-
     city: restaurant.city,
     mainCategory: get(restaurant, 'categorisation.primary.name'),
     editorial_rating: restaurant.editorial_rating,
@@ -16,8 +23,9 @@ const restaurantsCleaned = restaurants
     image_url: restaurant.image_url,
     latitude: restaurant.latitude,
     longitude: restaurant.longitude,
-    id_area: get(restaurant,'area.name','',),
+    id_area: get(restaurant, 'area.name', ''),
   }))
+  // filtrer pour les existant (non undefined)
   .filter(restaurant => (
     restaurant.id &&
     restaurant.name &&
@@ -28,44 +36,55 @@ const restaurantsCleaned = restaurants
     restaurant.description &&
     restaurant.image_url &&
     restaurant.latitude &&
-    restaurant.longitude&&
+    restaurant.longitude &&
     restaurant.id_area
   ))
+  //trier du plus grand au plus petit ranking
   .sort((a, b) => b.editorial_rating - a.editorial_rating)
-// console.log(restaurantsCleaned)
 
-Array.prototype.unique = function () {
-  return this.filter(function (value, index, self) {
-    return self.indexOf(value) === index;
-  });
-}
 
-const areasCleaned = restaurants
+//fonction d'arnaud pour verifie existance index
+// Array.prototype.unique = function () {
+//   return this.filter(function (value, index, self) {
+//     return self.indexOf(value) === index;
+//   });
+// }
+// console.log('Number of unique IDs', restaurantJsonFiltrer.map(restaurant => restaurant.id).unique().length)
+// console.log('Number of restaurants', restaurantJsonFiltrer.length)
+
+
+// creation d'un ojbet avec chaque area de chaque restaurant
+const creationObjetArea = restaurants
   .map(restaurant => ({
     area: get(restaurant, 'area.name'),
   })).filter(restaurant => (
     restaurant.area
   ));
-  console.log(areasCleaned)
 
-// const areasfilter = [];
-// for (let i = 0; i < areasCleaned.length; i++)
-//     for (let j = 0; j < areasfilter.length; j++)
-//     if (areasfilter[j] != areasCleaned[i].area) {
-//       areasfilter.push(areasCleaned[i])
-//   } else {
-//     console.log('tutu')
-//   }
-// console.log(areasfilter)
-const areaUnique = uniqWith(areasCleaned, isEqual)
+//suppression des doublons avec lodash puis ajout d'un index unique par area
+const areaFiltreDoublon = uniqWith(creationObjetArea, isEqual)
+const aeraComplet = areaFiltreDoublon.map((e, index) => ({
+  id: index, name: e.area
+}
+))
 
+//creation d'un nouveau tableau en reprenant chaque objet restaurant et en ajoutant un cle etrangere
+//correspondante a l id de son area 
+const restaurantComplet = [];
+restaurantJsonFiltrer
+  .map(e => {
+    aeraComplet.map(a => {
+      if (e.id_area === a.name) {
+        restaurantComplet.push({ ...e, id_area: a.id })
+      }
+    })
+  })
+// console.log(restaurantComplet)
 
-// console.log('Number of unique IDs', restaurantsCleaned.map(restaurant => restaurant.id).unique().length)
-// console.log('Number of restaurants', restaurantsCleaned.length)
-// console.log(areasCleaned)
-writeFile('restaurants-cleaned.json', JSON.stringify(restaurantsCleaned, null, '\t'))
+//creation de deux json restaurant et area
+writeFile('restaurants-cleaned.json', JSON.stringify(restaurantComplet, null, '\t'))
   .then(() => console.log('Wrote to file successfully'))
 
-writeFile('areas-cleaned.json', JSON.stringify(areaUnique, null, '\t'))
+writeFile('areas-cleaned.json', JSON.stringify(aeraComplet, null, '\t'))
   .then(() => console.log('Wrote to file successfully'))
 
